4a73af0a807cab54e65b71101e9545b372d031a8
Tobias Wenzel <Tobias_Wenzel@intuit.com>
2017-03-27 15:01:45 +0200

    uses new logger and adds command line options

--- prosper/crons/FetchMapInfo.py
+++ prosper/crons/FetchMapInfo.py
@@ -2,31 +2,24 @@
 
 from os import path #TODO: path->plumbum?
 
-import requests
 import pandas
-import xml.etree.ElementTree as ET
 import eveapi
 from plumbum import cli
 
-import prosper.common.prosper_logging as p_log
+import prosper.common.prosper_logging as p_logging
 from prosper.common.prosper_config import get_config
 
 HERE = path.abspath(path.dirname(__file__))
 ME = __file__.replace('.py', '')
 CONFIG_ABSPATH = path.join(HERE, 'cron_config.cfg')
 
-config = get_config(CONFIG_ABSPATH)
-logger = create_logger(
-    'debug-FetchMapInfo',
-    HERE,
-    config
-)
+CONFIG = get_config(CONFIG_ABSPATH)
 
 ## MAP INFO GLOBALS ##
-KILLS_FIELDS = config.get(ME, 'kills_fields')
-JUMPS_FIELDS = config.get(ME, 'jumps_fields')
-FAC_FIELDS = config.get(ME, 'fac_war_systems_fields')
-SOV_FIELDS = config.get(ME, 'sovereignty_fields')
+KILLS_FIELDS = CONFIG.get(ME, 'kills_fields')
+JUMPS_FIELDS = CONFIG.get(ME, 'jumps_fields')
+FAC_FIELDS = CONFIG.get(ME, 'fac_war_systems_fields')
+SOV_FIELDS = CONFIG.get(ME, 'sovereignty_fields')
 
 
 def convert_to_panda(
@@ -48,9 +41,33 @@ def refactor_to_docs(
 
 class XmlDriver(cli.Application):
 
+    _log_builder = p_logging.ProsperLogger(
+        ME,
+        HERE
+    )
+
+    debug = cli.Flag(
+        ['d', '--debug'],
+        help='Debug mode, no production db, headless mode'
+    )
+
+    @cli.switch(
+        ['-v', '--verbose'],
+        help='Enable verbose messaging'
+    )
+    def enable_verbose(self):
+        """toggle verbose logger"""
+        self._log_builder.configure_debug_logger()
+
+
     def main(self):
-        '''meat of script.  Logic runs here.  Write like step list'''
-        logger.info('Getting Map info')
+        """Program Main flow"""
+        global LOGGER
+        if not self.debug:
+            self._log_builder.configure_discord_logger()
+        LOGGER = self._log_builder.logger
+
+        LOGGER.info('Getting Map info')
 
         eveapi.set_user_agent("eveapi.py/1.3")
 
@@ -66,11 +83,13 @@ class XmlDriver(cli.Application):
         facWarSystemsDataframe = convert_to_panda(facWarSystems, FAC_FIELDS.split(','))
         sovereigntyDataframe = convert_to_panda(sovereignty, SOV_FIELDS.split(','))
 
-        logger.info('Saving to CSV File')
+        LOGGER.info('Saving to CSV File')
         killsDataframe.to_csv("kills.csv")
         jumpsDataframe.to_csv("jumps.csv")
         facWarSystemsDataframe.to_csv("facWarSystems.csv")
         sovereigntyDataframe.to_csv("sovereignty.csv")
+
+
 
 
 
