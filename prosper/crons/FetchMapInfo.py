d546d2187d3f542237490d7a1facd9cd2469486b
Tobias Wenzel <Tobias_Wenzel@intuit.com>
2017-03-27 14:45:42 +0200

    moved to eveapi library

--- prosper/crons/FetchMapInfo.py
+++ prosper/crons/FetchMapInfo.py
@@ -5,9 +5,10 @@ from os import path #TODO: path->plumbum?
 import requests
 import pandas
 import xml.etree.ElementTree as ET
+import eveapi
 from plumbum import cli
 
-from prosper.common.prosper_logging import create_logger
+import prosper.common.prosper_logging as p_log
 from prosper.common.prosper_config import get_config
 
 HERE = path.abspath(path.dirname(__file__))
@@ -22,62 +23,23 @@ logger = create_logger(
 )
 
 ## MAP INFO GLOBALS ##
-XML_BASE_URL = config.get(ME, 'XML_endpoint')
-XML_FILES = config.get(ME, 'files')
-
-
-def get_map_info_from_eve(
-    file
-):
-    url = XML_BASE_URL + file
-    try:
-        request = requests.get(
-            url
-        )
-    except Exception as err_msg:
-        logger.error(
-            'EXCEPTION: request failed' +
-            '\r\texception={0}'.format(err_msg) +
-            '\r\turl={0}'.format(url)
-        )
-        raise err_msg
-
-    if request.status_code == requests.codes.ok:
-        try:
-            response = request.content
-        except Exception as err_msg:
-            logger.error(
-                'EXCEPTION: unable to parse payload' +
-                '\r\texception={0}'.format(err_msg) +
-                '\r\turl={0}'.format(err_msg)
-            )
-            logger.debug(request.text)
-    else:
-        logger.error(
-            'EXCEPTION: bad status code' +
-            '\r\texception={0}'.format(request.status_code) +
-            '\r\turl={0}'.format(url)
-        )
-        logger.debug(request.text)
-        raise Exception('BAD STATUS CODE ' + str(request.status_code))
-
-    return response
+KILLS_FIELDS = config.get(ME, 'kills_fields')
+JUMPS_FIELDS = config.get(ME, 'jumps_fields')
+FAC_FIELDS = config.get(ME, 'fac_war_systems_fields')
+SOV_FIELDS = config.get(ME, 'sovereignty_fields')
 
 
 def convert_to_panda(
-        data
+        data,
+        fields
 ):
-    etree = ET.fromstring(data)
-    return pandas.DataFrame(refactor_to_docs(etree))
+    return pandas.DataFrame(refactor_to_docs(data, fields))
 
 def refactor_to_docs(
-        root):
-    fields = []
-    for rowset in root.iter('rowset'):
-        fields = rowset.get("columns").split(',')
-        logger.debug("found columns: " + str(fields))
+        root,
+        fields):
 
-    for child in root.iter('row'):
+    for child in root.solarSystems:
         row = dict()
         for field in fields:
             row[field] = child.get(field)
@@ -89,12 +51,28 @@ class XmlDriver(cli.Application):
     def main(self):
         '''meat of script.  Logic runs here.  Write like step list'''
         logger.info('Getting Map info')
-        for file in XML_FILES.split(','):
-            logger.info('Getting File: ' + file)
-            fetch_file = get_map_info_from_eve(file)
-            panda_dataframe= convert_to_panda(fetch_file)
-            logger.info('Saving to CSV File: ' + file)
-            panda_dataframe.to_csv(file + ".csv")
+
+        eveapi.set_user_agent("eveapi.py/1.3")
+
+        api = eveapi.EVEAPIConnection()
+
+        kills = api.map.kills()
+        jumps = api.map.Jumps()
+        facWarSystems = api.map.FacWarSystems()
+        sovereignty = api.map.Sovereignty()
+
+        killsDataframe = convert_to_panda(kills, KILLS_FIELDS.split(','))
+        jumpsDataframe = convert_to_panda(jumps, JUMPS_FIELDS.split(','))
+        facWarSystemsDataframe = convert_to_panda(facWarSystems, FAC_FIELDS.split(','))
+        sovereigntyDataframe = convert_to_panda(sovereignty, SOV_FIELDS.split(','))
+
+        logger.info('Saving to CSV File')
+        killsDataframe.to_csv("kills.csv")
+        jumpsDataframe.to_csv("jumps.csv")
+        facWarSystemsDataframe.to_csv("facWarSystems.csv")
+        sovereigntyDataframe.to_csv("sovereignty.csv")
+
+
 
 if __name__ == '__main__':
     XmlDriver.run()
